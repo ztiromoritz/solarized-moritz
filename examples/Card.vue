5545df885bea828115dd427d915aa45eb12fc20c
Moritz Rebbert <moritz.rebbert@udo.edu>
2021-09-17 23:37:11 +0200

    initial commit

---
+++ examples/Card.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="wrapper">
+    <div v-for="airport in airports" :key="airport.abbreviation" class="card">
+      <p>{{ airport.abbreviation }}</p>
+      <p>{{ airport.name }}</p>
+      <p>{{ airport.city }}, {{ airport.state }}</p>
+    </div>
+  </div>
+</template>
+<script setup>
+import { ref } from "vue";
+import data from "@/data/us-airports.js";
+
+export default {
+  name: "App",
+  setup() {
+    const airports = ref(data);
+    const regex = /[^x]*/g;
+    return { airports };
+  },
+};
+</script>
+<style lang="scss">
+.wrapper {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-column-gap: 1rem;
+  max-width: 960px;
+  margin: 0 auto;
+}
+
+.card {
+  border: 3px solid;
+  border-radius: 0.5rem;
+  padding: 1rem;
+  margin-bottom: 1rem;
+}
+
+.card p:first-child {
+  font-weight: bold;
+  font-size: 2.5rem;
+  margin: 1rem 0;
+}
+
+.card p:last-child {
+  font-style: italic;
+  font-size: 0.8rem;
+}
+</style>
